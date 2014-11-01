df5bc1d946e3f94b371678576d737439b653265f
mannanj <mannanjavid@gmail.com>
2014-11-01 16:58:33 -0400

    Baxter controller v1 for both arms.

--- Baxter_ARCHR/startController.sh
+++ Baxter_ARCHR/startController.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "======Starting window for servers. Run bash serverl1.sh, bash serverl2.sh======"
-cd /home/archr/ros_ws && gnome-terminal --tab --command "bash baxter.sh" --tab -e "bash baxter.sh"
-echo "======bash controllerl1.sh and bash controllerl2.sh separately here.======"
+echo "======Starting window for servers. Run bash servergripper.sh first in one window======"
+cd /home/archr/ros_ws && gnome-terminal --tab --command "bash baxter.sh" #--tab -e "bash baxter.sh"
+echo "======Then run bash controllerBoth.sh and bash gripper.sh in two additional windows======"
 cd /home/archr/ros_ws && gnome-terminal --tab --command "bash baxter.sh" --tab -e "bash baxter.sh"

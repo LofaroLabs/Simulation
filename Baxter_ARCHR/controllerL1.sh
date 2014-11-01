9158e70f33f0faddc32f0e9bc11d4c4ee6b969fc
mannanj <mannanjavid@gmail.com>
2014-11-01 16:51:26 -0400

    Baxter controller v1 for both arms.

--- Baxter_ARCHR/controllerL1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "======This should start the left arm controller======"
-cd /home/archr/projects/archr_Code/Baxter
-python controllerArmLeft.py -l left
--- Baxter_ARCHR/serverL1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-echo "======This should start the left arm server======"
-cd /home/archr/projects/archr_Code/Baxter
-python serverArms.py -l left
